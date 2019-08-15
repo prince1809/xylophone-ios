aff29cfd53f52e157f60e98f74d6df756a4b4b95
prince <prince18pat@gmail.com>
2019-08-15 20:38:38 +0900

    play different tone for different buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var player: AVAudioPlayer?
-
+    let soundArray = ["note1", "note2","note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        let selectedSoundFile : String = soundArray[sender.tag - 1]
+        playSound(selectedSoundFile)
+    }
+    
+    func playSound(_ selectedFile: String) {
+        let url = Bundle.main.url(forResource: selectedFile, withExtension: "wav")!
         do {
             player = try AVAudioPlayer(contentsOf: url)
             guard let player = player else {return}
@@ -29,7 +34,6 @@ class ViewController: UIViewController {
         } catch let error as NSError {
             print(error)
         }
-        print("Button pressed: ", sender.tag)
     }
 }
 
